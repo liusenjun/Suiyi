7d2d2e3b39719a38f9799b1a8303ef42e4e21a44
liusenjun <1160627451@qq.com>
2021-04-08 09:10:53 +0800

    代办页

---
+++ pages/daiban/daiban.js
@@ -0,0 +1,92 @@
+// pages/daiban/daiban.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     test:{
+       list:[
+         {
+          color:"#20a674",
+          month:'Nov',
+          day: 21,
+          ddl: '10:00:00',
+          topic:'主标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+         },
+
+         {
+          color:"#c54949",
+          month:'Dec',
+          day: 31,
+          ddl: '10:00:00',
+          topic:'主标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+         },
+
+         {
+          color:"#176095",
+          month:'Oct',
+          day: 12,
+          ddl: '10:00:00',
+          topic:'主标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'
+         }
+       ]
+     }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
